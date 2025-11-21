b28a0f6087f6e84c5f09b5c3480ebc817652e867
rahulnamilakonda <rahulnamilakonda100@gmail.com>
2025-11-22 00:00:21 +0530

    post state

--- states/post_state.py
+++ states/post_state.py
@@ -1,10 +1,13 @@
 import operator
-from typing import Annotated, List, TypedDict
+from typing import Annotated, List, Literal, TypedDict
 
 
-class PostSchema(TypedDict):
-    prompt: str
+class PostState(TypedDict):
+    topic: str
+    platform: Literal["LinkedIn", "X-(Twitter)", "Instagram"]
     prevPosts: Annotated[List[str], operator.add]
+    feedback: Annotated[List[str], operator.add]
     post: str
     iteration: int
+    status: Literal["approved", "refused"]
     max_iterations: int
