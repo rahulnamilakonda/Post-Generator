d65d1aca19cf4374cde7dfef5aa01be6c560bfd3
rahulnamilakonda <rahulnamilakonda100@gmail.com>
2025-11-22 00:35:28 +0530

    utils

--- utils/utils.py
+++ utils/utils.py
@@ -2,11 +2,10 @@ import os
 import uuid
 
 
-class Utils:
-    def writeToFile(self, post: str):
-        if not os.path.isdir("output"):
-            os.mkdir("output")
+def writeToFile(self, post: str):
+    if not os.path.isdir("output"):
+        os.mkdir("output")
 
-        filename = str(uuid.uuid1())
-        with open(f"output/{filename}", "w") as f:
-            f.writelines(post)
+    filename = str(uuid.uuid1())
+    with open(f"output/{filename}", "w") as f:
+        f.writelines(post)
